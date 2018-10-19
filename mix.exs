ad266d3c3f87487ceec742768ba3c95cedb0943a
Piotr Baj <piotr.baj@petabajt.net>
2018-10-19 15:34:25 +0200

    Coverage report with excoveralls

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule Webapp.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -41,7 +48,8 @@ defmodule Webapp.MixProject do
       {:phoenix_live_reload, "~> 1.2-rc", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:excoveralls, "~> 0.10", only: [:dev, :test]}
     ]
   end
 
