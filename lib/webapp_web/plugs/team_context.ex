5dbb37277cf2648223918266efbacf06a2572d59
Piotr Baj <piotr@petabajt.net>
2018-12-06 10:29:00 +0100

    Team context access, switchin teams

--- lib/webapp_web/plugs/team_context.ex
+++ lib/webapp_web/plugs/team_context.ex
@@ -11,17 +11,29 @@ defmodule WebappWeb.TeamContext do
   @doc """
   Set user namespace if there is no team namespace, based on current user.
   """
-  def call(%Plug.Conn{assigns: %{team: %Team{} = team}} = conn, _opts), do: conn
+  def call(%Plug.Conn{assigns: %{current_user: %User{} = user, team: %Team{} = team}} = conn, _opts) do
+    case Enum.filter(user.teams, fn user_team -> team.id == user_team.id end) do
+      [head | _] -> conn
+      [] -> conn
+            |> put_flash(:error, "You are not authorized to view this page")
+            |> redirect(to: build_path(conn.path_info) )
+    end
+  end
 
   def call(%Plug.Conn{assigns: %{current_user: %User{} = user}} = conn, _opts) do
     team = Accounts.user_first_team(user)
 
     conn
     |> assign(:team, team)
+    #|> redirect(to: build_path([team.namespace | conn.path_info]))
   end
 
   def call(%Plug.Conn{} = conn, _opts) do
     conn
     |> assign(:team, nil)
+  end
+
+  defp build_path(path_info) do
+    "/" <> Enum.join(path_info, "/")
   end
 end
--- lib/webapp_web/views/layout_view.ex
+++ lib/webapp_web/views/layout_view.ex
@@ -1,11 +1,17 @@
 defmodule WebappWeb.LayoutView do
   use WebappWeb, :view
 
+  alias Webapp.{Accounts.Team}
+
   def version do
     "#{Application.spec(:webapp, :vsn)}-#{Mix.Project.config()[:vcs_version]}"
   end
 
   def current_session(conn) do
     Plug.Conn.get_session(conn, :phauxth_session_id)
+  end
+
+  def switch_team_path(conn, %Team{} = team) do
+    "/" <> team.namespace <> "/" <> Enum.join(conn.path_info, "/")
   end
 end
