8bcf69a6dd2086365baac027c910b8e231c1c954
Mateusz Kwiatkowski <mateusz.kwiatkowski@dreamhost.com>
2019-01-09 11:31:00 +0100

    Get domain for webapp from environment

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :webapp, WebappWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "demo.runhyve.app", port: 443],
+  url: [host: System.get.env("WEBAPP_DOMAIN") || "demo.runhyve.app", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true
