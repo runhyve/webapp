bb4cf36b71eb00924c5eb8b379dbc375f2065ae9
Piotr Baj <piotr.baj@petabajt.net>
2020-01-04 02:40:11 +0100

    Hypervisor machines listing

--- lib/webapp_web/controllers/admin/machine_controller.ex
+++ lib/webapp_web/controllers/admin/machine_controller.ex
@@ -8,41 +8,11 @@ defmodule WebappWeb.Admin.MachineController do
     Hypervisors.Hypervisor
   }
 
-  plug :load_and_authorize_resource,
-    model: Machine,
-    non_id_actions: [:index, :create, :new],
-    preload: [:hypervisor, :plan, :networks, :distribution],
-    except: [:index]
-
   plug :load_resource,
     model: Hypervisor,
     id_name: "hypervisor_id",
     only: [:index],
-    preload: [:hypervisor_type, machines: Hypervisors.preload_active_machines],
-    required: true
-
-  def index(conn, %{"hypervisor_id" => hypervisor_id} = _params) do
-    hypervisor = conn.assigns[:hypervisor]
-
-    status =
-      case Hypervisors.update_hypervisor_status(hypervisor) do
-        {:ok, status} -> status
-        {:error, _} -> "unreachable"
-      end
-
-    conn =
-      if status == "unreachable" do
-        put_flash(conn, :error, "Hypervisor #{hypervisor.name} unreachable.")
-      else
-        conn
-      end
-
-    render(conn, "index.html",
-      machines: hypervisor.machines,
-      hypervisor: hypervisor,
-      status: status
-    )
-  end
+    preload: [:hypervisor_type, machines: Hypervisors.preload_active_machines]
 
   def index(conn, _params) do
     machines = Machines.list_machines([:hypervisor, :plan, :networks, :distribution])
