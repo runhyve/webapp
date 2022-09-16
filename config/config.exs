f57a3342090afac3a8e60509d61ffce790d9d05a
Piotr Baj <piotr.baj@petabajt.net>
2022-09-16 13:06:26 +0200

    Build all js files with esbuild

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,8 @@ config :webapp, WebappWeb.Endpoint,
 config :esbuild,
   version: "0.12.18",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args:
+      ~w(js/app.js js/admin.js js/hypervisor.js js/ip_pool.js js/machine.js js/team.js --bundle --target=es2016 --outdir=../priv/static/assets),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
